mg979/vim-visual-multi

commit 00b19c16d744b40baa2c3ed8db089488dd6cc147

Also find operator uses regions backup

autoload/vm/operators.vim
@@ -151,13 +151,13 @@ endfun
 fun! vm#operators#find(start, visual, ...) abort
     if a:start
         if !g:Vm.is_active
-            call s:init()
+            call s:backup_map_find()
             if a:visual
                 "use search register if just starting from visual mode
                 call s:V.Search.get_slash_reg(s:v.oldsearch[0])
             endif
         else
-            call s:init()
+            call s:backup_map_find()
         endif
 
         "ensure there is an active search
@@ -218,13 +218,7 @@ fun! vm#operators#find(start, visual, ...) abort
     endwhile
     let &wrapscan = ows
     let s:v.clear_vm_matches = 1
-
-    if !len(s:R())
-        call cursor(startline, startcol)
-        call s:F.exit('No matches found. Exiting VM.', 0)
-    else
-        call s:G.update_map_and_select_region()
-    endif
+    call s:merge_find()
 endfun
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -244,4 +238,23 @@ fun! s:old_updatetime() abort
         let &updatetime = g:Vm.oldupdate
     endif
 endfun
+
+fun! s:backup_map_find() abort
+    "use temporary regions, they will be merged later
+    call s:init()
+    let s:Bytes = copy(s:V.Bytes)
+    let s:V.Regions = []
+    let s:V.Bytes = {}
+    let s:v.index = -1
+    let s:v.no_search = 1
+    let s:v.eco = 1
+endfun
+
+fun! s:merge_find() abort
+    let new_map = copy(s:V.Bytes)
+    let s:V.Bytes = s:Bytes
+    call s:G.merge_maps(new_map)
+    unlet new_map
+endfun
+
 " vim: et ts=4 sw=4 sts=4 :

commit d3edee64b5654d5bcf24e93c8c40521241d5920c

Fix: ensure pattern is set for find operator
Broken in 00b19c1

autoload/vm/funcs.vim
@@ -80,7 +80,6 @@ endfun
 fun! s:Funcs.no_regions() abort
     if !len(s:R())
         let s:v.index = -1
-        call self.msg('No regions.')
         return 1
     endif
 endfun

autoload/vm/operators.vim
@@ -157,15 +157,10 @@ fun! vm#operators#find(start, visual, ...) abort
                 call s:V.Search.get_slash_reg(s:v.oldsearch[0])
             endif
         else
+            call s:V.Search.ensure_is_set()
             call s:backup_map_find()
         endif
 
-        "ensure there is an active search
-        if empty(s:v.search)
-            if !len(s:R()) | call s:V.Search.get_slash_reg()
-            else           | call s:V.Search.get()
-            endif
-        endif
 
         call s:updatetime()
         let s:v.finding = 1

autoload/vm/search.vim
@@ -76,10 +76,7 @@ fun! s:Search.get() abort
     endif
 
     "fallback to first region.txt or @/, if no active search
-    if !empty(s:v.search) | return
-    elseif len(s:R())     | call self.add(self.escape_pattern(s:R()[0].txt))
-    else                  | call self.get_slash_reg()
-    endif
+    if empty(s:v.search) | call self.ensure_is_set() | endif
 endfun
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -89,6 +86,17 @@ fun! s:Search.get_slash_reg(...) abort
     if a:0 | let @/ = a:1 | endif
     call s:update_search(self.get_pattern('/', 1))
     if empty(s:v.search) | call s:update_search(s:v.oldreg[1]) | endif
+endfun
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+fun! s:Search.ensure_is_set(...) abort
+    """Ensure there is an active search.
+    if empty(s:v.search)
+        if !len(s:R()) | call self.get_slash_reg()
+        else           | call self.add(self.escape_pattern(s:R()[0].txt))
+        endif
+    endif
 endfun
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""