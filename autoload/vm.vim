mg979/vim-visual-multi

commit b9c04f3ea9b3f0dbc2008106276a3f33b854885f

Rename plug for nohlsearch, improve behaviour
The previous system was causing a flash, use lazyredraw to prevent it

autoload/vm.vim
@@ -100,7 +100,7 @@ fun! vm#init_buffer(empty, ...) abort
         if &hlsearch && !v:hlsearch && !a:empty
             let s:v.oldhls = 1
             if !g:Vm.selecting
-                call feedkeys("\<Plug>(VM-Toggle-Hls)")
+                call vm#noh()
             endif
         else
             let s:v.oldhls = 0
@@ -135,7 +135,7 @@ fun! vm#reset(...)
     call vm#variables#reset()
 
     if s:v.oldhls
-        call feedkeys("\<Plug>(VM-Toggle-Hls)")
+        call feedkeys(vm#noh())
     endif
 
     call vm#commands#regex_reset()
@@ -180,6 +180,15 @@ fun! vm#reset(...)
     call vm#variables#reset_globals()
     delcommand VMSmartChange
     call garbagecollect()
+endfun
+
+"------------------------------------------------------------------------------
+
+fun! vm#noh() abort
+    """Turn off highlighting using lazyredraw.
+    if s:v.oldlz | return "\<Plug>(VM-Noh)"
+    else         | return "\<Plug>(VM-LazyNoh)"
+    endif
 endfun
 
 

autoload/vm/plugs.vim
@@ -108,7 +108,8 @@ fun! vm#plugs#buffer() abort
   nnoremap <silent>       <Plug>(VM-Remove-Every-n-Regions)  :<c-u>call vm#commands#remove_every_n_regions(v:count)<cr>
   nnoremap <silent>       <Plug>(VM-Show-Infoline)           :call b:VM_Selection.Funcs.infoline()<cr>
 
-  nnoremap <silent>       <Plug>(VM-Toggle-Hls)              :if v:hlsearch<bar>nohlsearch<bar>else<bar>set hls<bar>endif<cr>
+  nnoremap <silent>       <Plug>(VM-Noh)                     :nohlsearch<cr>
+  nnoremap <silent>       <Plug>(VM-LazyNoh)                 :set lz<bar>nohlsearch<bar>set nolz<cr>
 
   for m in g:Vm.motions
     exe "nnoremap <silent> <Plug>(VM-Motion-".m.") :\<C-u>call vm#commands#motion('".m."', v:count1, 0, 0)\<cr>"

autoload/vm/operators.vim
@@ -30,7 +30,7 @@ fun! vm#operators#get(cnt) abort
     """Perform a yank, the autocmd will create the region.
     let g:Vm.selecting = 1
     call s:init()
-    let hls = &hlsearch && !v:hlsearch ? "\<Plug>(VM-Toggle-Hls)" : ''
+    let hls = &hlsearch && !v:hlsearch ? vm#noh() : ''
     call s:updatetime()
     silent! nunmap <buffer> y
 

commit af2d29be8d57d471cb5a20d43edaf75a8e8fd45e

Fix nohlsearch (broken in b9c04f3)

autoload/vm.vim
@@ -100,7 +100,7 @@ fun! vm#init_buffer(empty, ...) abort
         if &hlsearch && !v:hlsearch && !a:empty
             let s:v.oldhls = 1
             if !g:Vm.selecting
-                call vm#noh()
+                call feedkeys(vm#noh())
             endif
         else
             let s:v.oldhls = 0

autoload/vm/plugs.vim
@@ -108,8 +108,8 @@ fun! vm#plugs#buffer() abort
   nnoremap <silent>       <Plug>(VM-Remove-Every-n-Regions)  :<c-u>call vm#commands#remove_every_n_regions(v:count)<cr>
   nnoremap <silent>       <Plug>(VM-Show-Infoline)           :call b:VM_Selection.Funcs.infoline()<cr>
 
-  nnoremap <silent>       <Plug>(VM-Noh)                     :nohlsearch<cr>
-  nnoremap <silent>       <Plug>(VM-LazyNoh)                 :set lz<bar>nohlsearch<bar>set nolz<cr>
+  nnoremap <silent>       <Plug>(VM-Noh)                     :if v:hlsearch<bar>nohlsearch<bar>else<bar>set hls<bar>endif<cr>
+  nnoremap <silent>       <Plug>(VM-LazyNoh)                 :set lz<bar>if v:hlsearch<bar>nohlsearch<bar>else<bar>set hls<bar>endif<bar>set nolz<cr>
 
   for m in g:Vm.motions
     exe "nnoremap <silent> <Plug>(VM-Motion-".m.") :\<C-u>call vm#commands#motion('".m."', v:count1, 0, 0)\<cr>"