mg979/vim-visual-multi

commit 5278b94c88fdd9b9c9a7482221b9afac1f6414ec

After a 'p' paste at cursors, move at end

autoload/vm/ecmds1.vim
@@ -159,8 +159,13 @@ fun! s:Edit.block_paste(before) abort
             let s = remove(text, 0)
             call s:F.set_reg(s)
 
-            if a:before | normal! P
-            else        | normal! p
+            if a:before
+                normal! P
+            else
+                normal! p
+                " update cursor position after the command
+                let diff = col('.') - r.a - 1
+                call r.shift(diff, diff)
             endif
 
             "update changed size

commit 4b8e769b52922a17de4ef0ddb7aeacc18386a246

fix #104 (move regions right)
broken by 5278b94

autoload/vm/ecmds1.vim
@@ -162,7 +162,9 @@ fun! s:Edit.block_paste(before) abort
                 normal! P
             else
                 normal! p
-                call r.update_cursor_pos()
+                if !exists('s:v.dont_move_cursors')
+                    call r.update_cursor_pos()
+                endif
             endif
 
             "update changed size
@@ -171,6 +173,7 @@ fun! s:Edit.block_paste(before) abort
             break
         endif
     endfor
+    silent! unlet s:v.dont_move_cursors
     call s:F.restore_reg()
 endfun " }}}
 

autoload/vm/ecmds2.vim
@@ -174,6 +174,7 @@ fun! s:Edit.shift(dir) abort
 
     call self.yank('"', 1)
     if a:dir
+        let s:v.dont_move_cursors = 1
         call self.paste(0, 0, 1, '"')
     else
         call self.delete(1, "_", 0, 0)