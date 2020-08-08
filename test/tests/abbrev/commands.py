b16ae436bfa766c23bd6404e28a9ea9fa31d79d8
mg979 <mg1979.git@gmail.com>
2020-08-08 12:17:07 +0200

    test: abbreviations

---
+++ test/tests/abbrev/commands.py
@@ -0,0 +1,36 @@
+# abbreviations in insert mode
+
+keys(r':inoreabbrev rr return\<CR>')
+
+keys(r':VMLive\<CR>')
+keys(r'4\<C-N>')
+keys('c')
+keys('rr')
+keys(r'\<Esc>')
+keys(r'\<Esc>')
+
+# the same, but with inverted g:VM_live_editing
+keys('6gg0')
+keys(r':VMLive\<CR>')
+keys(r'4\<C-N>')
+keys('c')
+keys('rr')
+keys(r'\<Esc>')
+keys(r'\<Esc>')
+
+# repeat both with replace mode
+keys('11gg0')
+keys(r':VMLive\<CR>')
+keys(r'3\<C-Down>')
+keys('R')
+keys('rr')
+keys(r'\<Esc>')
+keys(r'\<Esc>')
+
+keys('16gg0')
+keys(r':VMLive\<CR>')
+keys(r'3\<C-Down>')
+keys('R')
+keys('rr')
+keys(r'\<Esc>')
+keys(r'\<Esc>')
